e12da16e0c8f053fd61fb4320d9886bc7a399286
tfurru <96448736+tfurru@users.noreply.github.com>
2022-01-26 16:57:51 -0500

    Update watches.page.js

--- test/pages/watches.page.js
+++ test/pages/watches.page.js
@@ -1,36 +1,41 @@
 import Page from './page';
 
-class WatchesPage extends Page {
-  get promoBanner() {
-    return $('section.b-promobanner'); // findElement
-  }
-  get infoTitle() {
-    return $('.b-promobanner__info-title');
-  }
-  get shopButton() {
-    return $('.b-promobanner__info-btn');
-  }
-  get watchesCategoryList() {
-    return $$('section[id="s0-29-13-0-1[0]-0-0"] ul li'); // 6 elements
-  }
-  get fashionLink() {
-    return $$('.hl-cat-nav__js-tab a[href*="Fashion"]')[0];
-  }
-  get watchesLink() {
-    return $('.hl-cat-nav__sub-cat-col a[href*="Watches-Parts"]');
-  }
+class WatchesPage extends Page {   
+    get promoBanner() {
+        return $('#mainContent > section.b-module.b-promobanner.clearfix._FCF2BD5C1B05.d1img70 > div.b-promobanner__info');
+    }
+    get infoTitle() {
+        return $('#mainContent > section.b-module.b-promobanner.clearfix._FCF2BD5C1B05.d1img70 > div.b-promobanner__info > h2');
+    }
+    get shopButton() {
+        return $('#mainContent > section.b-module.b-promobanner.clearfix._FCF2BD5C1B05.d1img70 > div.b-promobanner__info > a');
+    }
+    get watchesCategoryList() {
+        return $$('section[id="s0-16-12-0-1[0]-0-0"] ul li');
+    }
+    get fashionLink() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(4) > a');
+    }
+    get watchesLink() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(4) > div.hl-cat-nav__flyout > div.hl-cat-nav__sub-cats > nav:nth-child(2) > ul > li:nth-child(1) > a');
+    }
+    get watchesHeader() {
+        return $('body > div.pagecontainer.srp-main--isLarge > div.pagecontainer__top > h1');
+    }
+    
 
-  open() {
-    super.open('/');
-  }
+    open() {
+        super.open('/');
+    }
 
-  getWatchesCategoryListText() {
-    const watchesList = [];
-    this.watchesCategoryList.map((element) =>
-      watchesList.push(element.getText())
-    );
-    return watchesList;
-  }
+    getWatchesCategoryListText() {
+        const watchesList = [];
+        this.watchesCategoryList.map((element) => 
+        watchesList.push(element.getText()));
+
+        return watchesList;
+        
+    }
 }
 
 export default new WatchesPage();
