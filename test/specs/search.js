4ae59dc44b01bc703c9d359b1e281a717aa2a09b
tfurru <96448736+tfurru@users.noreply.github.com>
2022-01-26 17:11:28 -0500

    Update search.js

--- test/specs/search.js
+++ test/specs/search.js
@@ -1,29 +1,26 @@
 import SearchPage from '../pages/search.page';
-import { waitForTextChange } from '../utilities/helper';
 import resources from '../resources';
-import allureReporter from '@wdio/allure-reporter';
+import { waitForTextChange } from '../utilities/helper';
 
 describe('Ebay Product Search', () => {
-  it('should open the main url and verify the title', () => {
-    SearchPage.open();
-    expect(browser).toHaveTitle(resources.homeTitle);
-  });
+    it('should open the main url and verify the title', () => {
+        SearchPage.open();
+        expect(browser).toHaveTitle(resources.homeTitle);
+    });
 
-  it('should search for a product and verify the search text value', () => {
-    allureReporter.addSeverity('Critical');
-    SearchPage.searchInput.addValue('Laptop');
-    SearchPage.searchBtn.click();
+    it('should search for a product and verify the search text value', () => {
+       SearchPage.searchInput.addValue('Laptop');
+       SearchPage.searchBtn.click();
 
-    expect(SearchPage.searchInput).toHaveValue('Laptop');
-  });
+        expect(SearchPage.searchInput).toHaveValue('laptop');
+    });
 
-  it('should redirect to a new page and verify the title', () => {
-    expect(browser).toHaveTitle('Laptop | eBay');
-  });
+    it('should redirect to a new page and verify the title', () => {
+        expect(browser).toHaveTitle(resources.laptopTitle);
+    });
 
-  it('should update the search category', () => {
-    allureReporter.addFeature('search category');
-    waitForTextChange(SearchPage.category, 'PC Laptops & Netbooks', 10000);
-    expect(SearchPage.category).toHaveText('PC Laptops & Netbooks');
-  });
+    it('should update the search category', () => {
+        waitForTextChange (SearchPage.category, resources.categoryTitle, 10000);
+        expect(SearchPage.category).toHaveText(resources.categoryTitle);
+    })
 });
