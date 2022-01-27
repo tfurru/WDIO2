51f3ac5b15668ecdda5b3cb57efbcfe8f9795525
tfurru <96448736+tfurru@users.noreply.github.com>
2022-01-26 20:54:24 -0500

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/search.js'
+        './test/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
